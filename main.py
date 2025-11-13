8ae750b213bd56c15cd320a2020f7cce696c7df2
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-11-13 01:57:38 -0600

    lector y exportador de hocr

--- main.py
+++ main.py
@@ -32,5 +32,23 @@ def instrucciones():
     for linea in file:
         st.markdown(linea)
 
+def hocr_csv(ruta_hocr, salida_csv):
+    
+    # Leemos el archivo HOCR
+    with open(ruta_hocr, "r", encoding="utf-8") as f:
+        sopa = BeautifulSoup(f, "html.parser")
+    
+    # Extraemos la info de cada palabra
+    data = []
+    for word in sopa.find_all("span", class_="ocr_line"):
+        text = word.get_text(strip=True)
+
+        data.append({"text": text})
+    
+    pd.DataFrame(data).to_csv(salida_csv, index=False)
+
 # -------------------------------- Interfaz (MAIN)-------------------------------------
 instrucciones()
+
+# Uso:
+hocr_csv("hocr/1E_1.hocr", "salida_1E_1.csv")
