106b0dd3459a2ce797ab53a55f287ac85e50abf2
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-11-13 00:22:13 -0600

    V.1.1.0 Estructura del proyecto

--- README.md
+++ README.md
@@ -3,6 +3,27 @@ La siguiente prueba tiene como objetivo evaluar el manejo de herramientas OCR pa
 
 ## Estructura general
 Se usará streamlit como una interfaz práctica para el usuario, además del servicio de despliegue.
+La estructura general del proyecto, hasta la versión V.1.1.0 es:
+```
+prueba_OCR/
+├── data/
+|   └── instrucciones.txt
+├── hocr/
+|   ├── 1C_5.hocr
+|   ├── 1E_1.hocr
+|   └── 3T_4.hocr
+├── recursos/
+|   ├── 1C.pdf
+|   ├── 1E.pdf
+|   ├── 3T.pdf
+|   ├── Prueba tecnica para desarrollador IA_ revisado.pdf
+|   └── notas.txt
+├── main.py
+└── README.md
+```
 
 ### V.1.0.0
 Se hace el principio de la interfaz y se monta el servicio de despliegue
+
+### V.1.1.0
+Se hace la estructura del proyecto en subcarpetas con recursos de apoyo como las vistas previas de PDF, las instrucciones revisadas, etc., además de los archivos .hocr y algunos datos como archivos de texto grandes que se leen en la interfaz, pero no están incrustados directamente en el código.
--- main.py
+++ main.py
@@ -10,11 +10,26 @@
 # ------------------------- Requerimientos y librerías -------------------------------
 import io
 import os
+import codecs
 import streamlit as st
 #from bs4 import BeautifulSoup
 
 # --------------------------- Seteadores ----------------------------------------------
-st.set_page_config(page_title="Lector de .hocr",
+st.set_page_config(page_title="Lector de HOCR",
                    page_icon = "👁️",
                    layout="wide")
-st.title("Lector de .hocr")
+st.title("Lector de HOCR")
+# --------------------------- Funciones -----------------------------------------------
+def instrucciones():
+    if not os.path.exists("data/instrucciones.txt"):
+        raise FileNotFoundError(f"El archivo {"data/instrucciones.txt"} no existe.")
+
+    with codecs.open("data/instrucciones.txt", "r", encoding="utf-8") as f:
+        fi = f.read()
+    file = fi.split('\n')
+
+    for linea in file:
+        st.markdown(linea)
+
+# -------------------------------- Interfaz (MAIN)-------------------------------------
+instrucciones()
